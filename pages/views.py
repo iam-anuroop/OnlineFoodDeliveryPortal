b84bf5821ab260461cd4fe42f81a4214b353ed31
iam-anuroop <anuroopku10@gmail.com>
2023-11-26 22:30:26 +0530

    nearest hotel

--- pages/views.py
+++ pages/views.py
@@ -14,10 +14,13 @@ from django.contrib.gis.measure import D
 
 class FilterNearHotels(APIView):
     def get(self, request):
-        latitude = request.data.get('latitude')
-        longitude = request.data.get('longitude')
+        latitude = request.query_params.get('latitude')
+        longitude = request.query_params.get('longitude')
+        print(latitude,longitude)
+        if latitude is None or longitude is None:
+            return Response({'error': 'Latitude and longitude are required in the query parameters'}, status=400)
         user_location = Point(float(longitude), float(latitude), srid=4326)
-        nearby_food_menus = FoodMenu.objects.filter(hotel__location__distance_lte=(user_location, D(km=4)),is_available=True)
+        nearby_food_menus = FoodMenu.objects.filter(hotel__location__distance_lte=(user_location, D(km=4000)),is_available=True)
         nearby_food_menus = nearby_food_menus.annotate(
             distance=Distance('hotel__location', user_location)
         ).order_by('distance')
