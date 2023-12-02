0790d19760695d6c8d91bacc9f986091304503ee
iam-anuroop <anuroopku10@gmail.com>
2023-12-02 16:21:16 +0530

    nothing

--- user_panel/models.py
+++ user_panel/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from hotel_panel.models import FoodMenu
-from accounts.models import UserProfile
+from accounts.models import MyUser
 
 
-
+# class Shopping(models.Model):
+#     user = models.ForeignKey(MyUser,on_delete=models.PROTECT)
 
 # Create your models here.
